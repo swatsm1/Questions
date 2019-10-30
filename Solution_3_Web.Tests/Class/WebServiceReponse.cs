cf19903dd72d1a5e3202bda51349397ebbfd7b23
swatsm1 <57162314+swatsm1@users.noreply.github.com>
2019-10-29 22:12:14 -0400

    Add files via upload

---
+++ Solution_3_Web.Tests/Class/WebServiceReponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Solution_3_Web
+{
+    public class WebServiceReponse
+    {
+        public int Status { get; set; }
+        public string Reason { get; set; }
+    }
+}
---
+++ Solution_3_Web.Tests/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Web.Script.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Solution_3_Web.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            //README 
+
+            //TO TEST START THE SOLUTION_3_WEB FIRST - IT WILL POINT TO A LOCALHOST (WILL OPEN BROWSER)
+            //THEN DEBUG THE TESTMETHOD Solution_3_Web.Tests.
+            //CHANGE THE VALUE OF THE XML FILE IN THE UNITTEST 
+
+            string stringfile = File.ReadAllText("XMLFile.xml");
+
+            string strNVPSandboxServer = string.Empty;
+                       
+            strNVPSandboxServer = "https://localhost:44374/WebService.ashx";
+            
+            string strNVP = stringfile;
+
+            //Create web request and web response objects, make sure you using the correct server (sandbox/live)
+             HttpWebRequest wrWebRequest = (HttpWebRequest)WebRequest.Create(strNVPSandboxServer);
+
+            wrWebRequest.Method = "POST";
+            wrWebRequest.ContentType = "application/text";
+
+            var requestWriter = new StreamWriter(wrWebRequest.GetRequestStream());
+
+            requestWriter.Write(strNVP);
+            requestWriter.Close();
+
+            // Get the response.
+            HttpWebResponse hwrWebResponse = (HttpWebResponse)(wrWebRequest.GetResponse());
+
+            var responseReader = new StreamReader(wrWebRequest.GetResponse().GetResponseStream());
+            
+            var responseData = responseReader.ReadToEnd();
+
+            WebServiceReponse objUsr = Deserialize<WebServiceReponse>(responseData);
+
+
+            Console.WriteLine("Status= " + objUsr.Status + " Reason= " + objUsr.Reason);
+
+        }
+
+        public T Deserialize<T>(string context)
+        {
+            string jsonData = context;
+
+            //cast to specified objectType
+            var obj = (T)new JavaScriptSerializer().Deserialize<T>(jsonData);
+            return obj;
+        }
+    }
+}
